425a4f4900a69f5c89c9324e2c69ac6707520c15
Ri787878 <ricardosdias@live.com>
2024-04-09 18:08:56 +0100

    testing stuff i guess

--- ddm_web_app/src/views/pages/ProfilePage/ProfilePage.js
+++ ddm_web_app/src/views/pages/ProfilePage/ProfilePage.js
@@ -1,9 +1,14 @@
-function ProfilePage() {
+import React from 'react';
+import styles from "./ProfilePage.module.css";
+
+function Profile() {
     return (
-        <div>
-            <h1>Profile Page</h1>
-        </div>
-    );
+		<div className = {styles.profileBox}>
+			<div className={styles.profileName}>
+				<h1 >Profile Name</h1>
+			</div>
+		</div>
+	);
 }
 
-export default ProfilePage
+export default Profile;
