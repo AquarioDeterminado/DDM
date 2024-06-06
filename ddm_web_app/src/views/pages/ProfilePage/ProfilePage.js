221a81d566e932341d9c803987ecdf5ba2fbf5c8
Ri787878 <ricardosdias@live.com>
2024-06-06 15:19:48 +0100

    working on the profile page making it better but not working i wanna die :(

--- ddm_web_app/src/views/pages/ProfilePage/ProfilePage.js
+++ ddm_web_app/src/views/pages/ProfilePage/ProfilePage.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import styles from "./ProfilePage.module.css";
-import SettingsButton from '../../components/SettingsButton/SettingsButton';
 import { ReactComponent as Background1 } from "../../assets/Player-Profile-Background.svg";
+import { ReactComponent as Button1 } from "../../assets/settings-button.svg";
+import {useNavigate} from "react-router-dom";
+import {ROUTES} from "../../MakeRoutes";
+import settingsButton from "../../components/SettingsButton/SettingsButton";
+
+
+function SettingsButton() {
+	const navigate = useNavigate();
+
+	function settingsButton() {
+		navigate(ROUTES.SETTINGSPAGE)
+	}
+
+	return (
+		<div className={styles.startButton} onClick={settingsButton}>
+			<Button1/>
+
+		</div>
+	);
+}
+
 
 function Profile() {
 	return (
@@ -10,32 +30,34 @@ function Profile() {
 			<div className={styles.profilePage}>
 				<div className={styles.profileBox}>
 					<div className={styles.profileInfo}>
-
+						<div className={styles.settingsButton}>
+							<SettingsButton />
+						</div>
+						<div className={styles.content}>
 							<h1>Puddle_04</h1>
 							<p>
 								Email: teste@gmail.com
-								<br />
+								<br/>
 								Location: Santos
 							</p>
 
-						<div className={styles.statsBox}>
-							<p>
-								Stats:
-								<div className={styles.statTable}>
-									Matches Played: 105
-									<br />
-									Matches Won: 73
-									<br />
-									Favourite Dog: Oscar
-									<br />
-									Worst Enemy: Mauricio_04
-								</div>
-							</p>
+							<div className={styles.statsBox}>
+								<p>
+									Stats:
+									<div className={styles.statTable}>
+										Matches Played: 105
+										<br/>
+										Matches Won: 73
+										<br/>
+										Favourite Dog: Oscar
+										<br/>
+										Worst Enemy: Mauricio_04
+									</div>
+								</p>
+							</div>
 						</div>
 					</div>
-					<div className={styles.settingsButton}>
-						<SettingsButton />
-					</div>
+
 				</div>
 			</div>
 		</div>
